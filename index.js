af2c56ea6eb3200808f350906954571ea1f6170e
Daris :D <dflamework@gmail.com>
2026-07-02 02:10:41 +0530

    corpora

---
+++ index.js
@@ -0,0 +1,52 @@
+// function test(){
+//     let age = 30;
+
+//     age = "Daris";
+
+//     console.log(age);
+// }
+
+// test();
+// console.log(age);
+
+// let a = "Daris";
+// console.log(typeof(a));
+
+//loops and strings
+// for(let i = 0; i<10; i++){
+//     console.log("Daris");
+// }
+
+// let b = `kdngikasndgh
+// ndkgknzshbk
+// modghosnhgon
+
+
+
+// asdgf`
+
+// //quotes print as is
+
+// console.log(b);
+
+// let name = new String("Daris");
+
+// name = "mahesh";
+// console.log(name);
+
+// console.log(b+" "+name);
+
+// console.log(name.substring(2));
+
+// let arr = ["Daris", 1, true];
+
+
+//declaring a object
+
+var myDog = {
+    "Name":"Rafe",
+    "Legs":4,
+    "Tail":2
+}
+
+console.log(myDog.Legs)
